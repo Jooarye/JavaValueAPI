eedb2729c39458cb08ec8d7603c288220e30c01b
Jooarye <42173961+Jooarye@users.noreply.github.com>
2018-10-18 11:14:17 +0200

    Added Configuration Function

--- src/de/jooarye/valueapi/ValueManager.java
+++ src/de/jooarye/valueapi/ValueManager.java
@@ -15,6 +15,9 @@ import java.util.Map;
 
 public class ValueManager {
 
+	/*
+	 * Registry Map to keep track of all Values
+	 */
 	private Map<Object, List<Value>> REGISTRY_MAP = new HashMap<Object, List<Value>>();
 
 	/*
@@ -30,14 +33,14 @@ public class ValueManager {
 	}
 
 	/*
-	 * Get all values by parent 
+	 * Get all values by parent
 	 */
 	public List<Value> getValues(Object parent) {
 		return this.REGISTRY_MAP.get(parent);
 	}
 
 	/*
-	 * Get all values by name 
+	 * Get all values by name
 	 */
 	public List<Value> getValues(String name) {
 		List<Value> temp = new ArrayList<Value>();
@@ -48,11 +51,58 @@ public class ValueManager {
 	}
 
 	/*
-	 * Get a specific value by name and parent 
+	 * Get a specific value by name and parent
 	 */
 	public Value getValue(String name, Object parent) {
 		return this.REGISTRY_MAP.get(parent).stream().filter(val -> val.getParent().equals(parent)).findFirst()
 				.orElse(null);
+	}
+
+	/*
+	 * Load a previously created Configuration File
+	 * Changes all Values to their previous value
+	 */
+	public void loadConfiguration(String path) throws IOException {
+		final Properties props = new Properties();
+		props.load(new FileInputStream(path));
+		this.REGISTRY_MAP.stream().forEach(value -> {
+			String key = value.getParent().getClass().getSimpleName() + "-" + value.getValue();
+			if(props.keySet().containsKey(key)) {
+				String val = props.getProperty(key);
+				this.setValue(value, val);
+			}
+		});
+	}
+
+	/*
+	 * Save the current Values as Configuration
+	 */
+	public void saveConfiguration(String path) throws IOException {
+		final Properties props = new Properties();
+		this.REGISTRY_MAP.stream().forEach(value -> {
+			String key = value.getParent().getClass().getSimpleName() + "-" + value.getValue();
+			props.setValue(key, String.valueOf(value.getValue()));
+		});
+		props.save(new FileOutputStream(path), "ValueApi Configuration File");
+	}
+
+	/*
+	 * Parse and set the value of a Value
+	 */
+	public void setValue(Value val, String value) {
+		if(val.getValue().getClass() == Integer.class) {
+			val.setValue(Integer.parseInt(value));
+		} else if(val.getValue().getClass() == Boolean.class) {
+			val.setValue(Boolean.parseBoolean(value));
+		} else if(val.getValue().getClass() == Float.class) {
+			val.setValue(Float.parseFloat(value));
+		} else if(val.getValue().getClass() == Double.class) {
+			val.setValue(Double.parseDouble(value));
+		} else if(val.getValue().getClass() == Long.class) {
+			val.setValue(Long.parseLong(value));
+		} else {
+			val.setValue(value);
+		}
 	}
 
 }
