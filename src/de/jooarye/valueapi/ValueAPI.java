21a42c31a92509839392887181640ad8f387a768
Jooarye <42173961+Jooarye@users.noreply.github.com>
2018-10-18 10:54:31 +0200

    Documentation Update

--- src/de/jooarye/valueapi/ValueAPI.java
+++ src/de/jooarye/valueapi/ValueAPI.java
@@ -3,7 +3,7 @@ package de.jooarye.valueapi;
 /*
  * Class: ValueAPI.java
  * Author: Jooarye
- * Description: Main API class
+ * Description: Main Api class
  * Date: 16.10.2018
  * License: GNU GPLv3
  */
@@ -12,8 +12,14 @@ public class ValueAPI {
 
 	public ValueManager valueManager;
 
+	/*
+	 * The instance for general use
+	 */
 	private static ValueAPI inst = new ValueAPI();
 
+	/*
+	 * Private constructor due to having a instance in this class
+	 */
 	private ValueAPI() {
 		this.valueManager = new ValueManager();
 	}
